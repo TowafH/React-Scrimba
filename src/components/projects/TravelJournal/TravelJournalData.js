3038728f2d01564b378b261f4c98163d9deec547
TowafH <towafh@nycstudents.net>
2024-11-28 19:05:08 -0500

    Add Data

---
+++ src/components/projects/TravelJournal/TravelJournalData.js
@@ -0,0 +1,41 @@
+// Images
+import fuji from './images/fuji.jpg'
+import sydney from './images/sydney.jpg'
+import geirangerfjord from './images/norway.jpg'
+import marker from './images/marker.webp'
+
+export default [
+    {
+        id: 1,
+        photo: fuji,
+        alt: "Mount Fuji",
+        title: "Mount Fuji",
+        icon: marker,
+        country: "Japan",
+        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        id: 2,
+        photo: sydney,
+        alt: "Sydney Opera House",
+        title: "Sydney Opera House",
+        icon: marker,
+        country: "Australia",
+        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        dates: "27 May, 2021 - 8 Jun, 2021",
+        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+    },
+    {
+        id: 3,
+        photo: geirangerfjord,
+        alt: "Geirangerfjord",
+        title: "Geirangerfjord",
+        icon: marker,
+        country: "Norway",
+        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+    },
+]
