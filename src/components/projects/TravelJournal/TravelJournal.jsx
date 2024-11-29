45aaadb64da1a4c1daf7133c9a56620004ef6ce5
TowafH <towafh@nycstudents.net>
2024-11-28 19:05:38 -0500

    Complete Travel Journal

--- src/components/projects/TravelJournal/TravelJournal.jsx
+++ src/components/projects/TravelJournal/TravelJournal.jsx
@@ -1,31 +1,53 @@
-// CSS
-import styles from './TravelJournal.module.css'
-
-// Images
-import fuji from './images/fuji.jpg'
-import marker from './images/marker.webp'
-
 // Component
 import Header from './components/Header'
 import Entry from './components/Entry.jsx'
 
+// Data
+import travelData from './TravelJournalData.js'
+
 function TravelJournal(){
+const entryElements = travelData.map((entry)=>{
+    return <Entry
+                // travelData.map((dataObject, index)=>{ 
+                // key={index}
+                // This will also work, not reccomended
+                key={entry.id}
+            // Using this method allows us to call the entire object and just modify the Component - #1
+                entry={entry}
+                //Also Written as: {...entry}
+                //Must remove .entry within the component #2.5
+
+
+            // When not passing Objects as props
+                // photo={entry.photo}
+                // alt={entry.alt}
+                // icon={entry.icon}
+                // title={entry.title}
+                // country={entry.country}
+                // googleMapsLink={entry.googleMapsLink}
+                // dates={entry.dates}
+                // text={entry.text}
+    />
+})
+
     return(
         <>
         <Header/>
-        <Entry 
-        photo={fuji} 
-        altImg="Mt. Fuji" 
-        icon={marker} 
-        country="Japan" 
-        location="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" 
-        landmark="Mount Fuji"
-        date="12 Jan, 2021 - 24 Jan, 20" 
-        desc="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."/>
-        <Entry/>
-        <Entry/>
+        {entryElements}
         </>
     )
 }
 
 export default TravelJournal
+
+        {/* <Entry 
+        photo={fuji} 
+        alt="Mt. Fuji" 
+        icon={marker} 
+        country="Japan" 
+        googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" 
+        title="Mount Fuji"
+        dates="12 Jan, 2021 - 24 Jan, 20" 
+        text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."/>
+        <Entry/>
+        <Entry/> */}
--- src/components/projects/TravelJournal/components/Entry.jsx
+++ src/components/projects/TravelJournal/components/Entry.jsx
@@ -2,20 +2,21 @@
 import styles from '../TravelJournal.module.css'
 
 function Entry(props){
+    // Now here in every instance we add a .entry infront of props to call the object with data - #2
     return(
         <>
         <div className={styles.entryContainer}>
             <div className={styles.photoContainer}>
-                <img src={props.photo} alt={props.altImg}/>
+                <img src={props.entry.photo} alt={props.entry.alt}/>
             </div>
 
             <div className={styles.titleContainer}>
-                <img src={props.icon}/>
-                <p className={styles.countryText}>{props.country}</p>
-                <a target="_blank" rel='noreferrer' href={props.location}><span>View on Google Maps</span></a>
-                <h1>{props.landmark}</h1>
-                <p className={styles.dateText}>{props.date}</p>
-                <p className={styles.descText}>{props.desc}</p>
+                <img src={props.entry.icon}/>
+                <p className={styles.countryText}>{props.entry.country}</p>
+                <a target="_blank" rel='noreferrer' href={props.entry.googleMapsLink}><span>View on Google Maps</span></a>
+                <h1>{props.entry.title}</h1>
+                <p className={styles.dateText}>{props.entry.dates}</p>
+                <p className={styles.descText}>{props.entry.text}</p>
             </div>
         </div>
         </>
